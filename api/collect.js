ba6a0055553f45cc61dec9b0dd175d8a55ddfe7b
gewenbo <wenbo@tiancai.pro>
2026-05-06 12:34:21 +0800

    Add Strategy OS to dashboard

--- api/collect.js
+++ api/collect.js
@@ -92,6 +92,7 @@ const PSYVERSE_MAP = {
   'knowledge-os.psyverse.fun':'knowledge-os',
   'influence-os.psyverse.fun':'influence-os',
   'uncertainty-os.psyverse.fun':'uncertainty-os',
+  'strategy-os.psyverse.fun':  'strategy-os',
   'emotion-os.psyverse.fun':'emotion-os',
   'blockchain-atlas.psyverse.fun':'blockchain-atlas',
   'ge.psyverse.fun':          'ge-clan-atlas',
