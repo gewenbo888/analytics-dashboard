8c50e9485578d355a51737771180a2cea846412c
gewenbo <wenbo@tiancai.pro>
2026-08-10 23:47:26 +0800

    Add world-changers to dashboard

--- api/collect.js
+++ api/collect.js
@@ -183,6 +183,7 @@ const PSYVERSE_MAP = {
   'hanzi-genesis.psyverse.fun': 'hanzi-genesis',
   'hanzi-genesis.vercel.app':   'hanzi-genesis',
   'x-playbook.psyverse.fun':      'x-playbook',
+  'world-changers.psyverse.fun':  'world-changers',
   'meta-civilization.psyverse.fun':'meta-civilization',
   'deep-stack.psyverse.fun':      'deep-stack',
   'electric-civilization.psyverse.fun': 'electric-civilization',
